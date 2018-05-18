4f2fdd450d49a26e71c4ac3ca243371fc6ee31e5
Aaron Marburg <amarburg@apl.washington.edu>
2018-05-18 15:25:07 -0700

    Corrected a few PEP8 warnings in fake_imu

--- yostlabs/fake_imu.py
+++ yostlabs/fake_imu.py
@@ -4,19 +4,20 @@ import logging
 import time
 import random
 
+
 class FakeImu:
 
-    def __init__(self,name):
+    def __init__(self, name):
         self.name = name
         self.logger = logging.getLogger(name)
         self.callbacks = []
 
-    def add_callback(self,callback):
+    def add_callback(self, callback):
         self.callbacks.append(callback)
 
     def run(self):
         while True:
-            ## New data has arrived!
+            # New data has arrived!
             euler = {'roll': random.random(),
                      'pitch': random.random(),
                      'yaw': random.random()}
