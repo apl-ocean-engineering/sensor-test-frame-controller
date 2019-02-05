2c42039039f40471904607668e6efbea495bb83f
Stefan Layanto <stef.layanto@gmail.com>
2019-02-05 13:44:30 -0800

    trying to add plotting

--- IMU/IMU_script.py
+++ IMU/IMU_script.py
@@ -11,6 +11,12 @@ import threading
 import time
 import sys
 import signal
+import csv
+import matplotlib
+matplotlib.use("tkAgg")
+import matplotlib.pyplot as plt
+import numpy as np
+
 
 # def run(name,q):
 #     while True:
@@ -33,7 +39,26 @@ def signal_handler(sig, frame):
         imu.stop_streaming()
     quit()
     #send_command_bytes_usb(chr(0x56))
-    
+
+plot_window = 20
+y_var = np.array(np.zeros([plot_window]))
+plt.ion()
+fig, ax = plt.subplots()
+line, = ax.plot(y_var)
+
+def plot_and_log(y_var, values = []):
+    with open("test_data.csv","a") as f:
+            writer = csv.writer(f,delimiter=",")
+            writer.writerow([time.time(),values[2]])
+    y_var = np.append(y_var,values[2])
+    y_var = y_var[1:plot_window+1]
+    line.set_ydata(y_var)
+    ax.relim()
+    ax.autoscale_view()
+    fig.canvas.draw()
+    fig.canvas.flush_events()
+    print(y_var)
+
 IMUs = []
 
 if __name__ == '__main__':
@@ -44,12 +69,13 @@ if __name__ == '__main__':
     t1 = threading.Thread(target=IMUs[0].start_stream_to_queue, args=(q1,))
     t1.start()
     signal.signal(signal.SIGINT, signal_handler)
- 
     while True:
         header1, data1 = q1.get()
         #TODO: Exit gracefully 
         if q1.not_empty:
-            print("% 9f,% 9f,% 9f,% 9f,% 9f,% 9f,% 9f" % tuple(data1) )
+            #print("% 9f,% 9f,% 9f,% 9f,% 9f,% 9f,% 9f" % tuple(data1) )
+            print(data1)
+            plot_and_log(y_var, data1)
     '''
     q2 = queue.Queue()
     t2 = threading.Thread(target=run, args = ("COM4", q2,))
