711b3682d978ff3728dfc44ccdf602112a34beca
Stefan Layanto <stef.layanto@gmail.com>
2019-02-28 12:01:38 -0800

    added ports

--- IMU/IMU_script.py
+++ IMU/IMU_script.py
@@ -12,10 +12,11 @@ import time
 import sys
 import signal
 import csv
+'''
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
-
+'''
 timer = time.time()
 
 # def run(name,q):
@@ -40,7 +41,7 @@ def signal_handler(sig, frame):
     quit()
     #send_command_bytes_usb(chr(0x56))
 
-
+'''
 def plot_and_log(time_data = [], values1 = [], values2 = [], values3 = []):
     global timer
     """
@@ -68,16 +69,20 @@ def plot_and_log(time_data = [], values1 = [], values2 = [], values3 = []):
     plt.cla()
 #    plt.show()
 # Potential Better way here: https://pythonprogramming.net/python-matplotlib-live-updating-graphs/
-
+'''
 IMUs = []
 
 if __name__ == '__main__':
     q1 = queue.Queue()
+    q2 = queue.Queue()
     print("Started")
     # global IMUs
-    IMUs.append(IMU("COM5", frequency=10))
+    IMUs.append(IMU("ttyS1", frequency=10))
+    IMUs.append(IMU("ttyS2", frequency=10))
     t1 = threading.Thread(target=IMUs[0].start_stream_to_queue, args=(q1,))
+    t2 = threading.Thread(target=IMUs[0].start_stream_to_queue, args=(q2,))
     t1.start()
+    t2.start()
     signal.signal(signal.SIGINT, signal_handler)
     full_data1 = []
     full_data2 = []
@@ -86,20 +91,25 @@ if __name__ == '__main__':
     plotting = True
     while True:
         header1, data1 = q1.get()
+        header2, data2 = q2.get()
         while q1.qsize() > 0:
             header1, data1 = q1.get() #Will be problematic when logging
+        while q2.qsize() > 0:
+            header2, data2 = q2.get() #Will be problematic when logging
         #TODO: Exit gracefully 
         if q1.not_empty:
-            #print("% 9f,% 9f,% 9f,% 9f,% 9f,% 9f,% 9f" % tuple(data1) )
+            print("data 1: ", "% 9f,% 9f,% 9f,% 9f,% 9f,% 9f,% 9f" % tuple(data1) )
             #print(data1)
             if plotting:
                 full_data1.append(data1[0])
                 full_data2.append(data1[1])
                 full_data3.append(data1[2])
                 time_data.append(time.time())
-                plot_and_log(time_data, full_data1, full_data2, full_data3)
-            print(str(q1.qsize()))
+                #plot_and_log(time_data, full_data1, full_data2, full_data3)
+            #print(str(q1.qsize()))
         #print("finishedPrinting")
+        if q2.not_empty:
+            print("data 2: ","% 9f,% 9f,% 9f,% 9f,% 9f,% 9f,% 9f" % tuple(data2) )
     '''
     q2 = queue.Queue()
     t2 = threading.Thread(target=run, args = ("COM4", q2,))
