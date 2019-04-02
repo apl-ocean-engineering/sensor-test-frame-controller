9923919396005538f14f32edbfd5d2f37995a95a
Stefan Layanto <stef.layanto@gmail.com>
2019-04-02 12:09:04 -0700

    undid change

--- IMU/IMU_script.py
+++ IMU/IMU_script.py
@@ -101,9 +101,14 @@ if __name__ == '__main__':
         print(IMUs[0].port_name + ": ", data[0], " normal: ", np.linalg.norm(data[0]))
         print(IMUs[1].port_name + ": ", data[1], " normal: ", np.linalg.norm(data[1]))
 
+        for imu in IMUs: # For when loop is slowed down by anything (like plotting)
+            while imu.q.qsize() > 0:
+                header1, data1 = imu.q.get()
+
         empty = [imus.q.empty() for imus in IMUs if True] #array of 1 if queue is empty, and 0 if not
-        print(empty)
-        if not all(empty):
+        #print(empty)
+        #if not all(empty):
+        if len(IMUs) ==  2:
             #print("data 1: ", "% 9f,% 9f,% 9f,% 9f,% 9f,% 9f,% 9f" % tuple(data1) )
             print("printing quaternions")
             for i in range(4) :
@@ -121,7 +126,3 @@ if __name__ == '__main__':
                 full_data4.append(relativeQuat[3])
                 time_data.append(time.time())
                 plot_and_log(time_data, full_data1, full_data2, full_data3, full_data4)
-
-        for imu in IMUs: # For when loop is slowed down by anything (like plotting)
-            while imu.q.qsize() > 0:
-                header1, data1 = imu.q.get()
