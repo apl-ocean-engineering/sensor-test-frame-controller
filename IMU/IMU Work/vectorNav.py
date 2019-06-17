04dd69d5b6ad57ccb5b2a3b772621fcf4364b6c4
Stefan Layanto <stef.layanto@gmail.com>
2019-06-17 15:57:01 -0700

    got cleaner reads using checks and multithreading. Got last gyro value

--- IMU/IMU Work/vectorNav.py
+++ IMU/IMU Work/vectorNav.py
@@ -6,8 +6,11 @@ import signal
 import io
 import time
 import csv
+import Queue
+import threading
 
 file_name  = "vectorNavData.csv"
+write = False
 
 def signal_handler(sig, frame):
     global running
@@ -16,39 +19,58 @@ def signal_handler(sig, frame):
 class VectorNav():
     def __init__(self, port_name, baud_rate=115200):
         self.port = serial.Serial(port_name, baud_rate, timeout=1.5)
+        self.q = Queue.Queue()
+        self.running = True
 
     def flushPort(self):
         self.port.flush()
 
     def hasData(self):
-        return self.port.in_waiting > 0
+        return self.port.in_waiting >= 122
 
     def getData(self):
         datum = self.port.readline().decode("utf-8")
-        datum = str(datum[7:]).strip('\r\n')
+        datum = str(datum[7:-5])
         datum = datum.split(',')
-        datum = datum[:-1]
-        try :
-            return [float(i) for i in datum]
-        except:
-            print("read error")
-            return None
+        # datum = datum[:-1]
+        if len(datum) == 12:
+            try :
+                return [float(i) for i in datum]
+            except:
+                print("VNav Read Error")
+        return None
+
+    def start(self):
+        t = threading.Thread(target=self.start_stream_to_queue)
+        t.start()
+    
+    def start_stream_to_queue(self):
+        while self.running:
+            try:
+                if self.hasData() :
+                    output = self.getData()
+                    if output != None:
+                        self.q.put(output)
+            except Exception as e:
+                print("Stopped reading from VNav:")
+                print(e)
+                self.running = False
 
 
 if __name__ == '__main__':
     signal.signal(signal.SIGINT, signal_handler)
     imu =  VectorNav("COM22")
     print("Running")
-    running = True
     time.sleep(1)
     file = open( file_name, 'w+' )
-    file.write("Time,Yaw,Pitch,Roll\n")
-
-    while running:
-        if imu.hasData() :
-            output = imu.getData()
-            # output = [str(time.time())] + output
+    file.write("Time,yaw,pitch,roll,magx,magy,magz,accelx,accely,accelz,gyrox,gyroy,gyroz\n")
+    imu.start()
+    while imu.running:
+        if imu.q.qsize() > 0:
+            output = imu.q.get()
+            while imu.q.qsize() > 0:
+                output = imu.q.get()
             print(output)
-            if file:
+            if file and write:
                 file.write(str(output) + '\n')
     file.close()
